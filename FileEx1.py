b74108c4557aa97855ceaab40fa41d15c534390c
SAKET KUMAR <njcusaket@gmail.com>
2024-02-28 22:45:47 -0500

    first commit

---
+++ FileEx1.py
@@ -0,0 +1,16 @@
+# open a file
+file1 = open("test1.txt", "r")
+
+# read the file
+content = file1.read()
+
+print(type(content))
+print(content)
+
+file1.close()
+
+
+with open("test2.txt", "r") as file2:
+    content = file2.read()
+    print(content)
+
